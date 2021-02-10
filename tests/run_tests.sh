e142ac14e351e1d3611239fb1d54f1e7839af3ca
GGuedesAB <gustavoguedesab@gmail.com>
2021-02-10 20:01:19 -0300

    Exit if incorrect test setup

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,6 +6,7 @@ if [ -z "$all_tests" ] || ! [ -f "$pyacc_exec" ]; then
     echo -e "Test set incomplete"
     echo -e "   Executable: $pyacc_exec"
     echo -e "   Tests: $all_tests"
+    exit 1
 fi
 for tst in $all_tests; do
     tst_output=$($pyacc_exec "$tst" 2>&1)
