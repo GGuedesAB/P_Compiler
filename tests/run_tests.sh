c191e8acb642b4d08b229ee9761b69504589ede6
GGuedesAB <gustavoguedesab@gmail.com>
2021-02-10 19:28:31 -0300

    Adding a couple of tests

---
+++ tests/run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+all_tests=$(find $PWD -type f -name "*.P");
+pyacc_exec=$(find $PWD -name "pyacc");
+if [ -z "$all_tests" ] || ! [ -f "$pyacc_exec" ]; then
+    echo -e "Test set incomplete"
+    echo -e "   Executable: $pyacc_exec"
+    echo -e "   Tests: $all_tests"
+fi
+for tst in $all_tests; do
+    tst_output=$($pyacc_exec "$tst" 2>&1)
+    if grep -q "Error" <<< "$tst_output"; then
+        echo -e "FAILURE: $tst failed:"
+        echo -e "   $tst_output"
+    fi
+done
